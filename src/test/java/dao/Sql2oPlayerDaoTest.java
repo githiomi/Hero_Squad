5628dafded69da32a4477a0a5459166ec8479aa1
Dhosio <danielgithiomi@gmail.com>
2020-04-29 17:12:37 +0300

    DAO test files added

---
+++ src/test/java/dao/Sql2oPlayerDaoTest.java
@@ -0,0 +1,23 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oPlayerDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void Player_canAddANewPlayer () {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
