02ed12bb5f703f91e25ed05476ecbe46fb7412a3
Dhosio <danielgithiomi@gmail.com>
2020-04-30 22:10:37 +0300

    added heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,6 +20,19 @@ public class App {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
 
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
